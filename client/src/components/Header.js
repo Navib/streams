061a7182da61fe72842e6dd56fd6edb2d50155bb
navib <ivan.baena.dev@gmail.com>
2019-11-13 21:36:28 -0500

    createStreams form setup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import GoogleAuth from './GoogleAuth';
-
+import GoogleAuth from "./GoogleAuth";
 
 const Header = props => {
   return (
